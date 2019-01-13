550a89127f6b3319f3fce3696ced80e237f3409c
yxpan <yxpan@whu.edu.cn>
2019-01-13 15:21:00 +0800

    refine install.sh: executables copy & PATH update

--- install.sh
+++ install.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
 # Install PANDA-PPP
 
-cd src && make && make install
+RED='\033[0;31m'
+BLUE='\033[0;34m'
+YELLOW='\033[1;33m'
+NC='\033[0m' # No Color
+
+cd src && make && make install \
+    && cd .. \
+    && cp -rf ./bin/ ${HOME}/.PANDA_BIN \
+    && echo "export PATH=${HOME}/.PANDA_BIN:$PATH" >> ${HOME}/.bashrc \
+    && source ${HOME}/.bashrc
+
+if [ $? -eq 0 ]; then
+    printf "${BLUE}::${NC} PANDA installation successfully completed!\n"
+    printf "${BLUE}::${NC} executable binaries are copy to ${HOME}/.PANDA_BIN\n"
+    printf "${BLUE}::${NC} ${HOME}/.PANDA_BIN added to PATH\n"
+else
+    printf "${RED}errror:${NC} PANDA installation failed!\n"
+fi
